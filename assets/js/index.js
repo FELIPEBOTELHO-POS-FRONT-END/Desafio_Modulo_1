ff15aac9e4c414ee042eadbde2d44f91fe7749c7
Felipe Botelho <felipebotelhofelipe@hotmail.com>
2022-06-01 14:14:44 -0300

    Carregando graficos Dashboard

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,19 +3,85 @@ import { getSummaryData } from "../js/api.js";
 init();
 
 async function init() {
-  await generateGlobalDataChart();
+  const data = await getSummaryData();
+  generateGlobalDataChart(data.Global);
+  generatePieChart(data.Global);
+  generateBarChart(data)
+  console.log(data);
 }
 
-async function generateGlobalDataChart() {
-  const data = await getSummaryData();
+function generateGlobalDataChart(data) {
   const confirmedDiv = document.getElementById("confirmed");
   const deathDiv = document.getElementById("death");
   const recoveredDiv = document.getElementById("recovered");
   const dateDiv = document.querySelector("#date > span");
-  confirmedDiv.textContent = (data.Global.TotalConfirmed).toLocaleString();
-  deathDiv.textContent = (data.Global.TotalDeaths).toLocaleString();
-  recoveredDiv.textContent = (data.Global.TotalRecovered).toLocaleString();
-  dateDiv.textContent= new Date(data.Global.Date).toLocaleString();
-  
+  confirmedDiv.textContent = (data.TotalConfirmed).toLocaleString();
+  deathDiv.textContent = (data.TotalDeaths).toLocaleString();
+  recoveredDiv.textContent = (data.TotalRecovered).toLocaleString();
+  dateDiv.textContent = new Date(data.Date).toLocaleString();
 }
+
+function generateBarChart(data) {
+  const datas = data.Countries.sort((a, b) => a.TotalDeaths > b.TotalDeaths ? -1 : a.TotalDeaths < b.TotalDeaths ? 1 : 0).slice(0, 10);
+
+  let bar = new Chart(document.getElementById("barras"), {
+    type: 'bar',
+    data: {
+      labels: datas.map(x => x.Country),
+      datasets: [
+        {
+          label: "Nº de Mortes",
+          data: datas.map(x => x.TotalDeaths),
+          backgroundColor: "purple"
+        }
+      ]
+    },
+    options: {
+      reponsive: true,
+      plugins: {
+        legend: {
+          position: 'top'
+        },
+        title: {
+          display: true,
+          text: "Total de Mortes por País - Top 10",
+          font: {
+            size: 25
+          }
+        }
+      }
+    }
+  });
+
+}
+
+function generatePieChart(data) {
+  let pizza = new Chart(document.getElementById("pizza"), {
+    type: 'pie',
+    data: {
+      labels: ["Recuperados", "Mortes", "Confirmados"],
+      datasets: [{
+        data: [data.NewRecovered, data.NewDeaths, data.NewConfirmed],
+        backgroundColor: ["blue", "yellow", "red"]
+      }
+      ]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top'
+        },
+        title: {
+          display: true,
+          text: "Distribuição de novos casos",
+          font: {
+            size: 25
+          }
+        }
+      }
+    }
+  })
+}
+
 
